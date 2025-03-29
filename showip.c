1717af25ab7faf6642b3690d77efcdbe190473bb
Cale Govindasami <calegovindasami@gmail.com>
2025-03-29 20:57:00 +0200

    showip - first steps...

---
+++ showip.c
@@ -0,0 +1,55 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+
+int main(int argc, char *argv[]) {
+    int status;
+    struct addrinfo hints;
+    struct addrinfo *servinfo, *p;
+    char ipstr[INET6_ADDRSTRLEN];
+
+    if (argc != 2) {
+        printf("Usage: showip hostname\n");
+        return 1;
+    }
+
+    memset(&hints, 0, sizeof(hints));
+
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if ((status = getaddrinfo(argv[1], "443", &hints, &servinfo)) != 0) {
+        printf("getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        void *addr;
+        char *ipver;
+
+        if (p->ai_family == AF_INET) {
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *) p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        } else {
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        printf(" %s: %s\n", ipver, ipstr);
+    }
+
+    freeaddrinfo(servinfo);
+
+    return 0;
+}
